be29feef68f441d29eecd7b1248115fb4e5fbc0e
mfnalex <1122571+mfnalex@users.noreply.github.com>
2023-08-29 23:44:44 +0200

    .

--- src/jmh/java/test/DistanceTest.java
+++ src/jmh/java/test/DistanceTest.java
@@ -1,50 +1,56 @@
+package test;
+
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+@State(Scope.Benchmark)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode({org.openjdk.jmh.annotations.Mode.AverageTime})
 public class DistanceTest {
 
     private record Pos(double x, double y, double z) {
-
         private double distanceSquared(Pos other) {
             double dx = x - other.x;
             double dy = y - other.y;
             double dz = z - other.z;
             return dx * dx + dy * dy + dz * dz;
         }
-
         private double distance(Pos other) {
-            return Math.sqrt(distanceSquared(other));
+            double dx = x - other.x;
+            double dy = y - other.y;
+            double dz = z - other.z;
+            return Math.sqrt(dx * dx + dy * dy + dz * dz);
         }
-
     }
-
-    private List<Pos> pos1 = new ArrayList<>();
-    private List<Pos> pos2 = new ArrayList<>();
-    private int nums = 10000;
-
+    private int nums = 1000;
+    private Pos[] pos1 = new Pos[nums];
+    private Pos[] pos2 = new Pos[nums];
     @Setup
     public void init() {
         for(int i = 0; i < nums; i++) {
-            pos1.add(new Pos(Math.random(), Math.random(), Math.random()));
-            pos2.add(new Pos(Math.random(), Math.random(), Math.random()));
+            pos1[i] = (new Pos(Math.random(), Math.random(), Math.random()));
+            pos2[i] = (new Pos(Math.random(), Math.random(), Math.random()));
         }
     }
-
     @Benchmark
     public void testDistance(Blackhole hole) {
         for(int i = 0; i < nums; i++) {
-            hole.consume(pos1.get(i).distance(pos2.get(i)));
+            hole.consume(pos1[i].distance(pos2[i]));
         }
     }
-
     @Benchmark
     public void testDistanceSquared(Blackhole hole) {
         for(int i = 0; i < nums; i++) {
-            hole.consume(pos1.get(i).distanceSquared(pos2.get(i)));
+            hole.consume(pos1[i].distanceSquared(pos2[i]));
         }
     }
 
